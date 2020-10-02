187e3d2ef47de4ca3014d7d539a478756622473d
MeetAhya11 <65812177+MeetAhya11@users.noreply.github.com>
2020-10-03 00:01:26 +0530

    Create stack.py

---
+++ stack.py
@@ -0,0 +1,15 @@
+stack = [] 
+stack.append(15) 
+stack.append(25) 
+stack.append(35) 
+  
+print('Initial stack') 
+print(stack) 
+  
+print('\nElements poped from stack:') 
+print(stack.pop()) 
+print(stack.pop()) 
+print(stack.pop()) 
+  
+print('\nStack after elements are poped:') 
+print(stack) 
